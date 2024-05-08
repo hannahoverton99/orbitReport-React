1e36cfa5897c847f104bfd7e64384da7c6191297
Hannah Overton <hannahoverton99@gmail.com>
2024-05-08 11:09:20 -0500

    Task 4 complete

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,22 @@
+import satData from './components.satData';
+
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    {orbitTypes.map((type, index) => {
+      return(
+        <div key={index}>
+      <button onClick = {() => filterByType(type)} key={index}>
+        {type} Orbit
+        </button>
+        </div>
+        );
+    })}
+      <button onClick = {() => setSat(satData)}>
+        All Orbits
+        </button>
     </div>
   );
-};
-
+   };
 export default Buttons;
